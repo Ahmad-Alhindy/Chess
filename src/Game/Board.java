92f4c9ab18a9838762ce8050d16bcd25c1865538
Ahmad Alhindy <alah23xc@student.ju.se>
2025-01-11 19:15:41 +0100

    change the board to a double array
    change the board to double array and caont it as the backend board

--- src/Game/Board.java
+++ src/Game/Board.java
@@ -3,18 +3,21 @@ package Game;
 import java.util.ArrayList;
 
 public class Board {
-	int[][] board = new int [8][8];
+	protected String[][] board = {
+		    {"♜", "♞", "♝", "♛", "♚", "♝", "♞", "♜"},
+		    {"♟", "♟", "♟", "♟", "♟", "♟", "♟", "♟"},
+		    {null, null, null, null, null, null, null, null},
+		    {null, null, null, null, null, null, null, null},
+		    {null, null, null, null, null, null, null, null},
+		    {null, null, null, null, null, null, null, null},
+		    {"♙", "♙", "♙", "♙", "♙", "♙", "♙", "♙"},
+		    {"♖", "♘", "♗", "♕", "♔", "♗", "♘", "♖"}
+		};
 	Piece pice; 
 	ArrayList<Piece> pieceList = new ArrayList<Piece>();
 	
 	
-	public Board() {
-		for (int i = 0; i < 8; ++i) {
-			for (int j = 0; j < 8; ++j) {
-				board[i][j] = j;
-			}
-		}
-	}
+	public Board() {}
 	
 	public void boardPrinter() {
 			for (int i = 0; i < 8; ++i) {
@@ -24,38 +27,39 @@ public class Board {
 				System.out.println();
 			}
 	}	
+	
+	
 	public void addPices() {
 		for (int i=1; i<9; ++i) {
-			Piece p = new Piece();
-			p.xPossition = i;
-			p.yPossition = 1;
-			p.pType(i);
-			p.team = Team.WHITE;
-			pieceList.add(p);	
-		}
-		for (int i=1; i<9; ++i) {
-			Piece p = new Piece();
-			p.xPossition = i;
-			p.yPossition = 8;
-			p.pType(i);
-			p.team = Team.BLACK;
-			pieceList.add(p);	
-		}
-		for (int i=1; i<9; ++i) {
-			Piece p = new Piece();
-			p.xPossition = i;
-			p.yPossition = 2;
-			p.pType(0);
-			p.team = Team.WHITE;
-			pieceList.add(p);	
-		}
-		for (int i=1; i<9; ++i) {
-			Piece p = new Piece();
-			p.xPossition = i;
-			p.yPossition = 7;
-			p.pType(0);
-			p.team = Team.BLACK;
-			pieceList.add(p);	
+			Piece pWhite = new Piece();
+			pWhite.xPossition = i;
+			pWhite.yPossition = 1;
+			pWhite.pType(i);
+			pWhite.team = Team.WHITE;
+			pieceList.add(pWhite);	
+			
+			Piece pBlack = new Piece();
+			pBlack.xPossition = i;
+			pBlack.yPossition = 8;
+			pBlack.pType(i);
+			pBlack.team = Team.BLACK;
+			pieceList.add(pBlack);
+			
+			Piece pPawnW = new Piece();
+			pPawnW.xPossition = i;
+			pPawnW.yPossition = 2;
+			pPawnW.pType(0);
+			pPawnW.team = Team.WHITE;
+			pieceList.add(pPawnW);	
+			
+			Piece pPawnB = new Piece();
+			pPawnB.xPossition = i;
+			pPawnB.yPossition = 7;
+			pPawnB.pType(0);
+			pPawnB.team = Team.BLACK;
+			pieceList.add(pPawnB);
 		}
 	}
+	
+	
 }
